5aa7e9e5ed7ad24b7c2f23c9a485976e7f927963
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-02-14 21:35:42 +0600

    update-about & read.me section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-        <h1>This is About section</h1>
+    <div style={{textAlign:'center'}}>
+      <h1>My Name is: Md Kaioum Islam</h1>
+      <p>
+        My goal in life is to be a person with real values and also to be a
+        problem solver who will not give up until all problems in life are
+        solved along with programming.
+      </p>
     </div>
   );
 };
